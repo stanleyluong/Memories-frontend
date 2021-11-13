a5778e56a39e0c5da112b6779ec694956b331937
Stanley Luong <slluong@uw.edu>
2021-11-13 04:35:20 -0800

    changed api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://stanleyluong-memories.herokuapp.com/posts';
+const url = 'https://stanley-memories.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url); 
 export const createPost = (newPost) => axios.post(url, newPost);
