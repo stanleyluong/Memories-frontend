6fd07fb10a9d105c86bd446ece1bdc0b860a7a73
Stanley Luong <slluong@uw.edu>
2021-11-17 03:19:56 -0800

    Updates

---
+++ precache-manifest.b7a3d3d4c4507a7ef979758a5812083a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5601848e96d188edec381cdf5061ac3",
+    "url": "/Memories/index.html"
+  },
+  {
+    "revision": "c29171bfa26bdf6e482d",
+    "url": "/Memories/static/css/main.894be846.chunk.css"
+  },
+  {
+    "revision": "2670ca21ef20d0d509d9",
+    "url": "/Memories/static/js/2.c7a977a7.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/Memories/static/js/2.c7a977a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29171bfa26bdf6e482d",
+    "url": "/Memories/static/js/main.246aa5be.chunk.js"
+  },
+  {
+    "revision": "9d07f6dc1eae4cb2a6ef",
+    "url": "/Memories/static/js/runtime-main.51ed09b2.js"
+  },
+  {
+    "revision": "18ebdb6d48b70ab2e72ac3bf9154d8be",
+    "url": "/Memories/static/media/memories-Text.18ebdb6d.png"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/Memories/static/media/memories.5c0c63fb.png"
+  }
+]);
